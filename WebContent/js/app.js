5f7b6e02475d2233b6bfc42898aec1bce0c1d3ef
Jordi Hernandez <jjordihm9@gmail.com>
2020-12-29 18:07:23 +0100

    add continents as options to the select continents element

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,8 +1,24 @@
 'use strict'
 
+// Elements from the document:
+const CONTINENTS_SELECT = document.querySelector('#continentDst');
+const COUNTRIES_SELECT = document.querySelector('#countryDst');
+
 // when the windows finish loading, execute init function
 window.onload = init();
 
 function init() {
 	clock();
+	addContinentsOptions();
+}
+
+/**
+ * Add options to the continent destination select element.
+ */
+function addContinentsOptions() {
+	// loop over each continent in the array
+	for (let continent of LOCATIONS.continents) {
+		// add the option to the select element
+		CONTINENTS_SELECT.options[CONTINENTS_SELECT.options.length] = new Option(continent.name, continent.name);
+	}
 }
