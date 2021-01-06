66a9ff0b772efd3b34b1bb32f845d2ac4b67c0d5
Jordi Hernandez <jjordihm9@gmail.com>
2021-01-06 23:13:48 +0100

    removed some countries from the list

--- WebContent/js/diccionary.js
+++ WebContent/js/diccionary.js
@@ -31,21 +31,6 @@ const LOCATIONS = {
 			'name': 'Africa',
 			'countries': [
 				{
-					'name': 'Algeria',
-					'price': 0,
-					'image': '/images/countries/algeria.jpg'
-				},
-				{
-					'name': 'Benin',
-					'price': 0,
-					'image': '/images/countries/benin.jpg'
-				},
-				{
-					'name': 'Cameroon',
-					'price': 0,
-					'image': '/images/countries/cameroon.jpg'
-				},
-				{
 					'name': 'Egypt',
 					'price': 0,
 					'image': '/images/countries/egypt.jpg'
@@ -86,11 +71,6 @@ const LOCATIONS = {
 					'image': '/images/countries/japan.jpg'
 				},
 				{
-					'name': 'Thailand',
-					'price': 0,
-					'image': '/images/countries/thailand.jpg'
-				},
-				{
 					'name': 'Turkey',
 					'price': 0,
 					'image': '/images/countries/turkey.jpg'
@@ -106,30 +86,10 @@ const LOCATIONS = {
 					'image': '/images/countries/australia.jpg'
 				},
 				{
-					'name': 'Fiji',
+					'name': 'New Zealand',
 					'price': 0,
-					'image': '/images/countries/fiji.jpg'
-				},
-				{
-					'name': 'New Zeland',
-					'price': 0,
-					'image': '/images/countries/new-zeland.jpg'
-				},
-				{
-					'name': 'Papua New Guinea',
-					'price': 0,
-					'image': '/images/countries/papua-new-guinea.jpg'
-				},
-				{
-					'name': 'Solomon Islands',
-					'price': 0,
-					'image': '/images/countries/solomon-islands.jpg'
-				},
-				{
-					'name': 'Tonga',
-					'price': 0,
-					'image': '/images/countries/tonga.jpg'
-				},
+					'image': '/images/countries/new-zealand.jpg'
+				}
 			]
 		},
 		{
@@ -176,21 +136,6 @@ const LOCATIONS = {
 					'image': '/images/countries/canada.jpg'
 				},
 				{
-					'name': 'Costa Rica',
-					'price': 0,
-					'image': '/images/countries/costa-rica.jpg'
-				},
-				{
-					'name': 'Honduras',
-					'price': 0,
-					'image': '/images/countries/honduras.jpg'
-				},
-				{
-					'name': 'Jamaica',
-					'price': 0,
-					'image': '/images/countries/jamaica.jpg'
-				},
-				{
 					'name': 'Mexico',
 					'price': 0,
 					'image': '/images/countries/mexico.jpg'
@@ -206,34 +151,14 @@ const LOCATIONS = {
 			'name': 'South America',
 			'countries': [
 				{
-					'name': 'Argentina',
-					'price': 0,
-					'image': '/images/countries/argentina.jpg'
-				},
-				{
 					'name': 'Brazil',
 					'price': 0,
 					'image': '/images/countries/brazil.jpg'
 				},
 				{
-					'name': 'Chile',
-					'price': 0,
-					'image': '/images/countries/chile.jpg'
-				},
-				{
 					'name': 'Peru',
 					'price': 0,
 					'image': '/images/countries/peru.jpg'
-				},
-				{
-					'name': 'Uruguay',
-					'price': 0,
-					'image': '/images/countries/uruguay.jpg'
-				},
-				{
-					'name': 'Venezuela',
-					'price': 0,
-					'image': '/images/countries/venezuela.jpg'
 				},
 			]
 		}
